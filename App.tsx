312dec31b957500ac036e822e2811b083e69f01c
Alan Junqueira <contato.alanjunqueira@gmail.com>
2023-05-20 17:50:52 -0300

    feat: add assets; install native-base

--- App.tsx
+++ App.tsx
@@ -1,14 +1,19 @@
-import { Text, View } from 'react-native';
+import { StatusBar, Text, View } from 'react-native';
+import { NativeBaseProvider } from 'native-base'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
   return (
-
-    <View>
+    <NativeBaseProvider>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       {fontsLoaded ? <Text>Hi !</Text> : <View />}
-    </View>
+    </NativeBaseProvider>
   );
 }
 
