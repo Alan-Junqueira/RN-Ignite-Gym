b6ad8ea2d04bbde44a5013aba0d5d3608bd4acb6
Alan Junqueira <contato.alanjunqueira@gmail.com>
2023-05-23 23:30:28 -0300

    feat, style: add conditional state to set color and border on Group component

--- src/components/Group.tsx
+++ src/components/Group.tsx
@@ -2,9 +2,10 @@ import { Text, Pressable, IPressableProps } from "native-base"
 
 interface IGroup extends IPressableProps {
   name: string
+  isActive: boolean
 }
 
-export const Group = ({ name, ...props }: IGroup) => {
+export const Group = ({ name, isActive, ...props }: IGroup) => {
   return (
     <Pressable
       mr={3}
@@ -15,6 +16,7 @@ export const Group = ({ name, ...props }: IGroup) => {
       justifyContent="center"
       alignItems="center"
       overflow="hidden"
+      isPressed={isActive}
       _pressed={{
         borderColor: "green.500",
         borderWidth: 1
@@ -22,7 +24,7 @@ export const Group = ({ name, ...props }: IGroup) => {
       {...props}
     >
       <Text
-        color="gray.200"
+        color={isActive ? "green.500" : "gray.200"}
         textTransform="uppercase"
         fontSize="xs"
         fontWeight="bold"
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,14 +1,25 @@
 import { Group } from '@components/Group'
 import { HomeHeader } from '@components/HomeHeader'
 import { HStack, VStack } from 'native-base'
+import { useState } from 'react';
 
 export const Home = () => {
+  const [groupSelected, setGroupSelected] = useState('costa');
+
   return (
     <VStack flex={1}>
       <HomeHeader />
       <HStack>
-        <Group name="costas" />
-        <Group name="ombro" />
+        <Group 
+          name="costa" 
+          isActive={groupSelected === 'costa'}
+          onPress={() => setGroupSelected('costa')}
+        />
+        <Group 
+          name="ombro" 
+          isActive={groupSelected === 'ombro'}
+          onPress={() => setGroupSelected('ombro')}
+        />
       </HStack>
     </VStack>
   )
