d3a188b9e95b7dc5adb89f73160276943739d8a0
Alan Junqueira <contato.alanjunqueira@gmail.com>
2023-05-23 22:22:29 -0300

    feat: add icons to bottom tab navigation

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -4,6 +4,11 @@ import { History } from '@screens/History'
 import { Home } from '@screens/Home'
 import { Profile } from '@screens/Profile'
 
+import HomeSvg from '@assets/home.svg'
+import HistorySvg from '@assets/history.svg'
+import ProfileSvg from '@assets/profile.svg'
+import { useTheme } from 'native-base'
+
 type TAppRoutes = {
   home: undefined
   exercise: undefined
@@ -16,6 +21,10 @@ export type TAppNavigatorRoutesProps = BottomTabNavigationProp<TAppRoutes>
 const { Navigator, Screen } = createBottomTabNavigator<TAppRoutes>()
 
 export const AppRoutes = () => {
+  const { sizes } = useTheme()
+
+  const iconSizes = sizes[6]
+
   return (
     <Navigator
       screenOptions={{
@@ -26,14 +35,23 @@ export const AppRoutes = () => {
       <Screen
         name="home"
         component={Home}
+        options={{
+          tabBarIcon: ({ color }) => <HomeSvg fill={color} width={iconSizes} height={iconSizes} />
+        }}
       />
       <Screen
         name="history"
         component={History}
+        options={{
+          tabBarIcon: ({ color }) => <HistorySvg fill={color} width={iconSizes} height={iconSizes} />
+        }}
       />
       <Screen
         name="profile"
         component={Profile}
+        options={{
+          tabBarIcon: ({ color }) => <ProfileSvg fill={color} width={iconSizes} height={iconSizes} />
+        }}
       />
       <Screen
         name="exercise"
