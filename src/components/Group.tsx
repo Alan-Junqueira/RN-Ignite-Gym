0d749cbb18861925265dcc46ab210bfe3d009034
Alan Junqueira <contato.alanjunqueira@gmail.com>
2023-05-23 23:24:30 -0300

    feat: implement ad style Pressable component on Group component; add Group component to Home screen

--- src/components/Group.tsx
+++ src/components/Group.tsx
@@ -1,18 +1,34 @@
-import { Text } from "native-base"
+import { Text, Pressable, IPressableProps } from "native-base"
 
-interface IGroup {
+interface IGroup extends IPressableProps {
   name: string
 }
 
-export const Group = ({ name }: IGroup) => {
+export const Group = ({ name, ...props }: IGroup) => {
   return (
-    <Text
-      color="gray.200"
-      textTransform="uppercase"
-      fontSize="xs"
-      fontWeight="bold"
-    >{
-        name}
-    </Text>
+    <Pressable
+      mr={3}
+      w={24}
+      h={10}
+      bg="gray.600"
+      rounded="md"
+      justifyContent="center"
+      alignItems="center"
+      overflow="hidden"
+      _pressed={{
+        borderColor: "green.500",
+        borderWidth: 1
+      }}
+      {...props}
+    >
+      <Text
+        color="gray.200"
+        textTransform="uppercase"
+        fontSize="xs"
+        fontWeight="bold"
+      >{
+          name}
+      </Text>
+    </Pressable>
   )
 }
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,12 +1,15 @@
 import { Group } from '@components/Group'
 import { HomeHeader } from '@components/HomeHeader'
-import { VStack } from 'native-base'
+import { HStack, VStack } from 'native-base'
 
 export const Home = () => {
   return (
     <VStack flex={1}>
       <HomeHeader />
-      <Group name="costas" />
+      <HStack>
+        <Group name="costas" />
+        <Group name="ombro" />
+      </HStack>
     </VStack>
   )
 }
