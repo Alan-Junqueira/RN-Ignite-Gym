8e245238e62d6c1f5350a2146aad32b43d331db3
Alan Junqueira <contato.alanjunqueira@gmail.com>
2023-05-24 23:08:04 -0300

    feat: implemente navigation between screens Exercise and Home

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,9 +1,33 @@
-import { Center, Text } from 'native-base'
+import { Center, Icon, Text, VStack } from 'native-base'
+import { TouchableOpacity } from 'react-native'
+import { Feather } from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/native'
+import { TAppNavigatorRoutesProps } from '@routes/app.routes'
 
 export const Exercise = () => {
+  const navigation = useNavigation<TAppNavigatorRoutesProps>()
+  const handleGoBack = () => {
+    navigation.goBack()
+  }
+
   return (
-    <Center flex={1}>
-      <Text>Exercise</Text>
-    </Center>
+    <VStack flex={1}>
+      <VStack
+        px={8}
+        bg="gray.600"
+        pt={12}
+      >
+        <TouchableOpacity
+          onPress={handleGoBack}
+        >
+          <Icon
+            as={Feather}
+            name="arrow-left"
+            color="green.500"
+            size={6}
+          />
+        </TouchableOpacity>
+      </VStack>
+    </VStack >
   )
 }
