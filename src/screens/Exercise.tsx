f7cf4eb00a206325e81238c03a7db671168111af
Alan Junqueira <contato.alanjunqueira@gmail.com>
2023-05-24 23:13:26 -0300

    feat: finalize header from Exercise screen

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,8 +1,10 @@
-import { Center, Icon, Text, VStack } from 'native-base'
+import { Center, HStack, Heading, Icon, Text, VStack } from 'native-base'
 import { TouchableOpacity } from 'react-native'
 import { Feather } from '@expo/vector-icons'
 import { useNavigation } from '@react-navigation/native'
 import { TAppNavigatorRoutesProps } from '@routes/app.routes'
+
+import BodySvg from '@assets/body.svg'
 
 export const Exercise = () => {
   const navigation = useNavigation<TAppNavigatorRoutesProps>()
@@ -27,6 +29,30 @@ export const Exercise = () => {
             size={6}
           />
         </TouchableOpacity>
+        <HStack
+          justifyContent="space-between"
+          mt={4}
+          mb={8}
+          alignItems="center"
+        >
+          <Heading
+            color="gray.100"
+            fontSize="lg"
+            flexShrink={1}
+          >
+            Puxada frontal
+          </Heading>
+          <HStack alignItems="center">
+            <BodySvg />
+            <Text
+              color="gray.200"
+              ml={1}
+              textTransform="capitalize"
+            >
+              Costas
+            </Text>
+          </HStack>
+        </HStack>
       </VStack>
     </VStack >
   )
