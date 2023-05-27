5636cdd19979802df21a3e4c769d471fdb7d6dbd
Alan Junqueira <contato.alanjunqueira@gmail.com>
2023-05-27 17:40:37 -0300

    feat: treating erros on signIn

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,4 +1,4 @@
-import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base"
+import { VStack, Image, Text, Center, Heading, ScrollView, useToast } from "native-base"
 
 import LogoSvg from "@assets/logo.svg"
 import BackgroundImg from "@assets/background.png"
@@ -11,6 +11,7 @@ import { useForm, Controller } from "react-hook-form"
 import { yupResolver } from "@hookform/resolvers/yup"
 import * as yup from 'yup'
 import { useAuth } from "@hooks/useAuth"
+import { AppError } from "@utils/AppError"
 
 type TSignUpInputs = {
   email: string
@@ -24,6 +25,7 @@ const signInSchema = yup.object({
 
 export const SignIn = () => {
   const navigation = useNavigation<TAuthNavigatorRoutesProps>()
+  const toast = useToast()
 
   const { signIn } = useAuth()
 
@@ -37,7 +39,18 @@ export const SignIn = () => {
 
   const handleSignIn = async (data: TSignUpInputs) => {
     const { email, password } = data
-    await signIn(email, password)
+    try {
+      await signIn(email, password)
+    } catch (error) {
+      const isAppError = error instanceof AppError
+
+      const title = isAppError ? error.message : 'Não foi possível entrar. Tente novamente mais tarde.'
+      toast.show({
+        title,
+        placement: 'top',
+        bgColor: "red.500"
+      })
+    }
   }
 
   return (
